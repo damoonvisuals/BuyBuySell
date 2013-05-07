bd551522575b0b51115aa138896e5cd3ef678dbf
Dasun Wang <damoon.sun@gmail.com>
2013-05-07 13:48:31 -0700

    CSS work on listing index

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -37,7 +37,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_limit => [310, 168]
+    process :resize_to_limit => [308, 369]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
